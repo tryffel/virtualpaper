d4c4e2dc892c8348f88d12645a88ff0bbcdd7bfe
Tero Vierimaa <tero.vierimaa@iki.fi>
2022-05-07 12:45:53 +0300

    e2e test: add metadata values

--- e2e/init.go
+++ e2e/init.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"encoding/json"
+	"fmt"
 	"gopkg.in/h2non/baloo.v3"
 	"testing"
 	"tryffel.net/go/virtualpaper/api"
@@ -33,8 +34,6 @@ func (t *httpTest) IsJson() *httpTest {
 
 var test = &httpTest{client: baloo.New("http://localhost:8000")}
 
-//var test = baloo.New("http://localhost:8000")
-
 func apiTest(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
@@ -55,6 +54,11 @@ func addMetadata(t *testing.T) {
 
 	addMetadataKey(t, "test-1", "testing")
 	addMetadataKey(t, "test-2", "testing another")
+
+	addMetadataKeyValues(t, 1, "value-1")
+	addMetadataKeyValues(t, 1, "value-2")
+	addMetadataKeyValues(t, 2, "value-10")
+	addMetadataKeyValues(t, 2, "value-11")
 	metadataAdded = true
 }
 
@@ -69,6 +73,22 @@ func addMetadataKey(t *testing.T, key, comment string) {
 		Comment: comment,
 	}
 
+	t.Log("add metadata key", key)
 	test.IsJson().Authorize().client.Post("/api/v1/metadata/keys").BodyString(jsonToBody(body)).
 		SetHeader("content-type", "application/json").Expect(t).Status(200).Done()
+}
+
+func addMetadataKeyValues(t *testing.T, keyId int, value string) {
+	body := api.MetadataValueRequest{
+		Value:          value,
+		Comment:        "",
+		MatchDocuments: false,
+		MatchType:      "",
+		MatchFilter:    "",
+	}
+
+	t.Log("add metadata key-value", keyId, value)
+
+	test.IsJson().Authorize().client.Post(fmt.Sprintf("/api/v1/metadata/keys/%d/values", keyId)).
+		BodyString(jsonToBody(body)).Expect(t).Status(200).Done()
 }
