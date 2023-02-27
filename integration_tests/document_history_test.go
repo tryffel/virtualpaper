a5e179789fa16e52b984f67b33d223d369636307
Tero Vierimaa <tero.vierimaa@iki.fi>
2023-02-27 21:17:11 +0200

    Tests for metadata history

--- integration_tests/document_history_test.go
+++ integration_tests/document_history_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	"testing"
 	"time"
+	"tryffel.net/go/virtualpaper/models"
 )
 
 func TestDocumentHistory(t *testing.T) {
@@ -82,4 +83,51 @@ func (suite *DocumentHistoryTestSuite) TestChangeDescription() {
 	assert.Equal(suite.T(), (*history)[1].NewValue, doc.Description)
 }
 
-// todo: metadata
+func (suite *DocumentHistoryTestSuite) TestChangeMetadata() {
+	history := getDocumentHistory(suite.T(), suite.userHttp, testDocumentX86.Id, 200)
+	assert.Len(suite.T(), *history, 1)
+
+	key1 := AddMetadataKey(suite.T(), suite.userHttp, "key", "", 200)
+	key2 := AddMetadataKey(suite.T(), suite.userHttp, "key2", "", 200)
+
+	value1 := AddMetadataValue(suite.T(), suite.userHttp, key1.Id, &models.MetadataValue{Value: "value1"}, 200)
+	value2 := AddMetadataValue(suite.T(), suite.userHttp, key2.Id, &models.MetadataValue{Value: "value1"}, 200)
+	value3 := AddMetadataValue(suite.T(), suite.userHttp, key2.Id, &models.MetadataValue{Value: "value2"}, 200)
+
+	doc := getDocument(suite.T(), suite.userHttp, testDocumentX86.Id, 200)
+	doc.Metadata = []models.Metadata{
+		{
+			KeyId: key1.Id, ValueId: value1.Id, Key: "", Value: "",
+		}}
+	updateDocument(suite.T(), suite.userHttp, doc, 200)
+	doc = getDocument(suite.T(), suite.userHttp, testDocumentX86.Id, 200)
+	doc.Metadata =
+		[]models.Metadata{
+			{
+				KeyId: key2.Id, ValueId: value2.Id, Key: "", Value: "",
+			},
+			{
+				KeyId: key2.Id, ValueId: value3.Id, Key: "", Value: "",
+			},
+		}
+	updateDocument(suite.T(), suite.userHttp, doc, 200)
+
+	history = getDocumentHistory(suite.T(), suite.userHttp, testDocumentX86.Id, 200)
+	assert.Len(suite.T(), *history, 5)
+
+	assert.Equal(suite.T(), (*history)[1].Action, "add metadata")
+	assert.Equal(suite.T(), (*history)[1].OldValue, "")
+	assert.Equal(suite.T(), (*history)[1].NewValue, fmt.Sprintf(`{"key_id":%d,"value_id":%d}`, key1.Id, value1.Id))
+
+	assert.Equal(suite.T(), (*history)[2].Action, "remove metadata")
+	assert.Equal(suite.T(), (*history)[2].OldValue, fmt.Sprintf(`{"key_id":%d,"value_id":%d}`, key1.Id, value1.Id))
+	assert.Equal(suite.T(), (*history)[2].NewValue, "")
+
+	assert.Equal(suite.T(), (*history)[3].Action, "add metadata")
+	assert.Equal(suite.T(), (*history)[3].OldValue, "")
+	assert.Equal(suite.T(), (*history)[3].NewValue, fmt.Sprintf(`{"key_id":%d,"value_id":%d}`, key2.Id, value2.Id))
+
+	assert.Equal(suite.T(), (*history)[4].Action, "add metadata")
+	assert.Equal(suite.T(), (*history)[4].OldValue, "")
+	assert.Equal(suite.T(), (*history)[4].NewValue, fmt.Sprintf(`{"key_id":%d,"value_id":%d}`, key2.Id, value3.Id))
+}
