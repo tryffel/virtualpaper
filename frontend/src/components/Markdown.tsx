1dcdaebfeca4e2bca2dc0b0b3c47a7f0aecc9fa0
Tero Vierimaa <tero.vierimaa@iki.fi>
2024-01-23 10:23:26 +0200

    Fix markdown editor

--- frontend/src/components/Markdown.tsx
+++ frontend/src/components/Markdown.tsx
@@ -16,52 +16,29 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Labeled, useInput, useRecordContext } from "react-admin";
-import { useController } from "react-hook-form";
-import * as Showdown from "showdown";
-//import "react-mde/lib/styles/css/react-mde-all.css";
-
-//import ReactMde from "react-mde";
+import {Labeled, useInput, useRecordContext, useTheme} from "react-admin";
+import {Box} from "@mui/material";
+import MDEditor from '@uiw/react-md-editor';
 
 export const MarkdownField = (props: any) => {
   const record = useRecordContext(props);
-
   return <ReactMarkdown>{record[props.source]}</ReactMarkdown>;
 };
 
-// Adapted from github.com/maluramichael/ra-input-markdown, adding support for RA4. Originally licensed under MIT.
 export const MarkdownInput = (props: any) => {
-  const { onChange, onBlur, ...rest } = props;
-  const {
-    field,
-    fieldState: { isTouched, invalid, error },
-    formState: { isSubmitted },
-  } = useInput({onChange, onBlur, ...props});
-
-  const [text, setText] = React.useState("- original texten");
-
-  const converter = new Showdown.Converter({
-    simplifiedautolink: true,
-    strikethrough: true,
-    tasklists: true
-  });
-  
-  const [selectedTab, setSelectedTab] = React.useState<"write" | "preview">("write");
+  const theme = useTheme()
+  const { onChange, onBlur} = props;
+  const { field} = useInput({onChange, onBlur, ...props});
   return (
-    <Labeled label={props.label}>
-      <></>
-      {/*
-      <ReactMde
-        value={field.value}
-        onChange={field.onChange}
-        selectedTab={selectedTab}
-        onTabChange={setSelectedTab}
-        generateMarkdownPreview={(md) => Promise.resolve(converter.makeHtml(md))}
-      />
-      */}
+    <Labeled label={props.label} fullWidth>
+      <Box data-color-mode={theme}>
+        <MDEditor
+            value={field.value}
+            onChange={field.onChange}
+            preview="edit"
+          />
+      </Box>
     </Labeled>
-
   );
 };
